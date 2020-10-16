7aa58542b8b536964dc01ac8b880a04c30317de5
Richard Boyes <rboyes@gmail.com>
2020-10-16 10:49:21 +0100

    Initial commit of R studio setup for centrica

---
+++ notebooks/Users/richard.boyes@centrica.com/install-rstudio.py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+script = """#!/bin/bash
+
+set -euxo pipefail
+RSTUDIO_BIN="/usr/sbin/rstudio-server"
+
+
+if [[ ! -f "$RSTUDIO_BIN" && $DB_IS_DRIVER = "TRUE" ]]; then
+  apt-get -o Acquire::http::proxy=false update
+  apt-get -o Acquire::http::proxy=false install -y gdebi-core
+  cd /tmp
+  # You can find new releases at https://rstudio.com/products/rstudio/download-server/debian-ubuntu/.
+  wget https://download2.rstudio.org/server/trusty/amd64/rstudio-server-1.2.5001-amd64.deb
+  sudo gdebi -n rstudio-server-1.2.5001-amd64.deb
+  rstudio-server restart || true
+fi
+"""
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("/databricks/rstudio")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/databricks/rstudio")
+
+# COMMAND ----------
+
+dbutils.fs.put("/databricks/rstudio/rstudio-install.sh", script, True)
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/databricks/rstudio/rstudio-install.sh"))
+
+# COMMAND ----------
+
